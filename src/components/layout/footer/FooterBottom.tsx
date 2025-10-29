8b6ec713b382ec8de12516db0a520b0b9faee4fe
mohamdeesayed <mohamdeesayed@outlook.com>
2025-10-29 17:03:23 +0300

    fix system app build

--- src/components/layout/footer/FooterBottom.tsx
+++ src/components/layout/footer/FooterBottom.tsx
@@ -12,17 +12,17 @@ const FooterBottom: React.FC = () => (
     className="border-t border-gray-300 mt-10 pt-4 text-sm text-center text-gray-600"
   >
     <p>
-      © 2018–2025 Dome Group. All rights reserved. DOME and its logo are trademarks of Dome Group of Companies.
+      © 2025 QSG Group. All rights reserved. QSG and its logo are trademarks of QSG Group of Companies.
     </p>
     <p className="mt-2">
       Design by{" "}
       <a
-        href="https://www.redspider.ae/"
+        href="#"
         target="_blank"
         rel="noopener noreferrer"
         className="text-gold hover:underline"
       >
-        Red Spider
+        Mohamed Reiad
       </a>
     </p>
   </motion.div>
